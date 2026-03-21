ce2280da1e76bfde12ca840018306360e5ff4d53
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-03-21 12:10:24 +0300

    feat : add seach bar component to seach recipe by name

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import recipes from './data/recipes.json'
 import styles from './App.module.css'
 import RecipeList from './components/RecipeList/RecipeList.jsx'
+import SearchBar from './components/SearchBar/SearchBar.jsx'
 
 export default function App() {
   const [orderedRecipes, setOrderedRecipes] = useState(recipes)
+  const [searchQuery, setSearchQuery] = useState('')
+  const filteredRecipes = orderedRecipes.filter((item) => {
+    return item.name.toLowerCase().includes(searchQuery.toLowerCase())
+  })
 
   function handleToggleOrder() {
     setOrderedRecipes((prev) => [...prev].reverse())
   }
+
+  useEffect(() => {
+  }, [searchQuery])
+  
 
   return (
     <div className={styles.app}>
@@ -24,8 +33,10 @@ export default function App() {
           </button>
         </div>
       </header>
+
       <main className={styles.main}>
-        <RecipeList recipes={orderedRecipes} />
+        <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>
+        <RecipeList recipes={filteredRecipes} />
       </main>
     </div>
   )
